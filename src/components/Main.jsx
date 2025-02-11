66e91667e8b11c9dafb3d600919bce628d3685b3
Diana Avila <missernightangel@gmail.com>
2025-02-11 20:52:16 -0300

    up

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,10 +1,8 @@
-
-
 const Main = () => {
   return (
     <div className="main">
       <div className="main__texts">
-        <h2>Artistas Populares</h2>
+        <h2>Artistas populares</h2>
         <a href="/" className="main__link">
           Mostrar tudo
         </a>
