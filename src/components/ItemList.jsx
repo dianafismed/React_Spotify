f0cc5e0dc360d855cbb189a8bd9464d2007a5ac7
Diana Avila <missernightangel@gmail.com>
2025-02-11 22:26:30 -0300

    up

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,36 +1,21 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCirclePlay } from "@fortawesome/free-regular-svg-icons";
+import SingleItem from "./SingleItem";
 
 const ItemList = () => {
   return (
     <div className="item-list">
       <div className="item-list__header">
         <h2>Artistas populares</h2>
-        <a href="/" className="item-list__link">
+        <a className="item-list__link" href="/">
           Mostrar tudo
         </a>
       </div>
-      <div className=".item-list__container">
-        <div className="single-item">
-          <div className="single-item__div-image-button">
-            <div className="single-item__div-image">
-              <img className="single-item__image" src="" alt="" />
-            </div>
 
-            <FontAwesomeIcon
-              className="single-item__icon"
-              icon={faCirclePlay}
-            />
-          </div>
-          <div className="single-item__texts">
-            <div className=".single-item__2lines">
-              <p className="single-item__title">
-                Nome nomelllllllllllllllllllllllllllllllllllllllllllllll
-              </p>
-            </div>
-            <p className="single-item__type">Artista</p>
-          </div>
-        </div>
+      <div className="item-list__container">
+        <SingleItem />
+        <SingleItem />
+        <SingleItem />
+        <SingleItem />
+        <SingleItem />
       </div>
     </div>
   );
