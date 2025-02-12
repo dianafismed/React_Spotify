dc6a3f73535f140565af068230035be4e84dacc9
Diana Avila <missernightangel@gmail.com>
2025-02-11 21:35:13 -0300

    up

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,3 +1,6 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCirclePlay } from "@fortawesome/free-regular-svg-icons";
+
 const Main = () => {
   return (
     <div className="main">
@@ -8,10 +11,62 @@ const Main = () => {
             Mostrar tudo
           </a>
         </div>
-        <div className="single-item">
-          <img className="single-item__image" src="" alt="" />
-          <p>Nome do Artista</p>
-          <p>Artista</p>
+
+        <div className=".item-list__container">
+          <div className="single-item">
+            <div className="single-item__div-image-button">
+              <div className="single-item__div-image">
+                <img className="single-item__image" src="" alt="" />
+              </div>
+
+              <FontAwesomeIcon
+                className="single-item__icon"
+                icon={faCirclePlay}
+              />
+            </div>
+
+            <div className="single-item__texts">
+              <div className=".single-item__2lines">
+                <p className="single-item__title">
+                  Nome nomelllllllllllllllllllllllllllllllllllllllllllllll
+                </p>
+              </div>
+              <p className="single-item__type">Artista</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="item-list">
+        <div className="item-list__header">
+          <h2>Músicas populares</h2>
+          <a href="/" className="item-list__link">
+            Mostrar tudo
+          </a>
+        </div>
+
+        <div className=".item-list__container">
+          <div className="single-item">
+            <div className="single-item__div-image-button">
+              <div className="single-item__div-image">
+                <img className="single-item__image" src="" alt="" />
+              </div>
+
+              <FontAwesomeIcon
+                className="single-item__icon"
+                icon={faCirclePlay}
+              />
+            </div>
+
+            <div className="single-item__texts">
+              <div className=".single-item__2lines">
+                <p className="single-item__title">
+                  Nome nomelllllllllllllllllllllllllllllllllllllllllllllll
+                </p>
+              </div>
+              <p className="single-item__type">Artista</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
