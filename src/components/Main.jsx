19213880ad056b8ecb4adb5adc3fd016fcdbd4e5
Diana Avila <missernightangel@gmail.com>
2025-02-11 21:06:03 -0300

    up

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,11 +1,18 @@
 const Main = () => {
   return (
     <div className="main">
-      <div className="main__texts">
-        <h2>Artistas populares</h2>
-        <a href="/" className="main__link">
-          Mostrar tudo
-        </a>
+      <div className="item-list">
+        <div className="item-list__header">
+          <h2>Artistas populares</h2>
+          <a href="/" className="item-list__link">
+            Mostrar tudo
+          </a>
+        </div>
+        <div className="single-item">
+          <img className="single-item__image" src="" alt="" />
+          <p>Nome do Artista</p>
+          <p>Artista</p>
+        </div>
       </div>
     </div>
   );
