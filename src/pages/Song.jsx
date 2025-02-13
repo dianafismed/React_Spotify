590fefa96f66553801c511d07b1b5c3e6051a402
Diana Avila <missernightangel@gmail.com>
2025-02-12 22:50:45 -0300

    up

--- src/pages/Song.jsx
+++ src/pages/Song.jsx
@@ -1,5 +1,34 @@
+import { Link } from "react-router-dom";
+import Player from "../components/Player";
+
 const Song = () => {
-  return <div>Song</div>;
+  return (
+    <div className="song">
+      <div className="song__container">
+        <div className="song__image-container">
+          <img src="" alt="Imagem da música" />
+        </div>
+      </div>
+
+      <div className="song__bar">
+        <Link to="/artist/1" className="song__artist-image">
+          <img
+            src="https://i.scdn.co/image/ab67616d00001e02af41105c5cd91b28f2cf219d"
+            alt=""
+            width={75}
+            height={75}
+          />
+        </Link>
+
+        <Player />
+
+        <div>
+          <p className="song__name">Nome da musica</p>
+          <p>Nome do Artista</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Song;
