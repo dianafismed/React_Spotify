825f94476c41ad1897060001049681e797f436fb
Diana Avila <missernightangel@gmail.com>
2025-02-16 18:29:25 -0300

    up

--- front-end/api/api.js
+++ front-end/api/api.js
@@ -4,9 +4,10 @@
 import "dotenv/config";
 import axios from "axios";
 
-const { NODE_ENV } = process.env;
+//const { NODE_ENV } = process.env;
 
-const URL = NODE_ENV === "development" ? "http://localhost:3000/api" : "/api";
+//const URL = NODE_ENV === "development" ? "http://localhost:3000/api" : "/api";
+const URL = "http://localhost:3000/api";
 
 const responseArtists = await axios.get(`${URL}/artists`);
 const responseSongs = await axios.get(`${URL}/songs`);
