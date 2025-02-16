dd06c25d0632333f968fc38790326d2510a221da
Diana Avila <missernightangel@gmail.com>
2025-02-16 18:04:38 -0300

    up

--- back-end/api/server.js
+++ back-end/api/server.js
@@ -1,6 +1,9 @@
 import express from "express";
 import cors from "cors"; // é um middleware
 import { db } from "./connect.js";
+import path from "path";
+
+const __dirname = path.resolve();
 
 const app = express();
 const PORT = 3000;
@@ -8,16 +11,22 @@ const PORT = 3000;
 app.use(cors());
 
 // Requisições
-app.get("/", (req, res) => {
+app.get("/api/", (req, res) => {
   res.send("Olá Mundo !");
 });
 
 // Busca os dados no Banco de Dados  --  é uma promisse, então preciso usar o await e async
-app.get("/artists", async (req, res) => {
+app.get("/api/artists", async (req, res) => {
   res.send(await db.collection("artists").find({}).toArray());
 });
-app.get("/songs", async (req, res) => {
+app.get("/api/songs", async (req, res) => {
   res.send(await db.collection("songs").find({}).toArray());
+});
+
+// Rota para não desenvolvedor
+app.use(express.static(path.join(__dirname, "../front-end/dist")));
+app.get("*", async (req, res) => {
+  res.send(path.join(__dirname, "../front-end/dist"));
 });
 
 // Seleciono a porta que será usada
